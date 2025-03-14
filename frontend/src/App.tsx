0c534d2de64317552c8be0e3fcaf83f480a8291f
RAKESH13595 <47477434+RAKESH13595@users.noreply.github.com>
2025-03-14 14:44:28 +0530

    shadcnui setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <>Welcome to To-DO App</>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
