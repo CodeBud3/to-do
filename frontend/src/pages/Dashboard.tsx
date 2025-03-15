711a02f73643b3261653b184f6999400ad287945
RAKESH13595 <47477434+RAKESH13595@users.noreply.github.com>
2025-03-16 03:34:29 +0530

    Update dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Dashboard() {
-  return <>Dashboard page</>;
+  return (
+    <>
+      Dashboard page
+      <Link to="/login">Sign in</Link>
+      <Link to="/signup">Sign up</Link>
+    </>
+  );
 }
