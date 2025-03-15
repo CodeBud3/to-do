4f59b9acf5341c31483fcf945d96ed66ee5bc8ad
RAKESH13595 <47477434+RAKESH13595@users.noreply.github.com>
2025-03-16 03:24:59 +0530

    Update auth.tsx

--- frontend/src/api/auth.tsx
+++ frontend/src/api/auth.tsx
@@ -6,7 +6,7 @@ import {
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "https://to-do-test-cjnr.onrender.com";
+  import.meta.env.VITE_API_URL || "https://to-do-test-cjnr.onrender.com/api/auth";
 
 export const register = async (
   credentials: SignUpCredentials
