21781647deaca5bfd92120ed8d29f338593e8eae
RAKESH13595 <47477434+RAKESH13595@users.noreply.github.com>
2025-03-15 14:09:29 +0530

    update model first name and last name

--- backend/src/validators/auth.validators.ts
+++ backend/src/validators/auth.validators.ts
@@ -2,7 +2,8 @@ import { z } from "zod";
 
 export const registerSchema = z.object({
   body: z.object({
-    name: z.string().min(1, "Name is required"),
+    firstName: z.string().min(1, "First name is required"),
+    lastName: z.string().min(1, "Last name is required"),
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, "Password must be at least 6 characters long"),
   }),
