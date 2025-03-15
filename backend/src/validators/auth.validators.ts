d42297846913a439a8d905cea561a62c5da73abe
RAKESH13595 <47477434+RAKESH13595@users.noreply.github.com>
2025-03-16 01:40:26 +0530

    handle field validations backend

--- backend/src/validators/auth.validators.ts
+++ backend/src/validators/auth.validators.ts
@@ -1,17 +1,18 @@
 import { z } from "zod";
+import { emailValidator, nameValidator, passwordValidator } from "./validators";
 
 export const registerSchema = z.object({
   body: z.object({
-    firstName: z.string().min(1, "First name is required"),
-    lastName: z.string().min(1, "Last name is required"),
-    email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password must be at least 6 characters long"),
+    firstName: nameValidator,
+    lastName: nameValidator,
+    email: emailValidator,
+    password: passwordValidator,
   }),
 });
 
 export const loginSchema = z.object({
   body: z.object({
-    email: z.string().email("Invalid email address"),
-    password: z.string().min(1, "Password is required"),
+    email: emailValidator,
+    password: passwordValidator,
   }),
 });
---
+++ backend/src/validators/validators.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const nameValidator = z
+  .string()
+  .trim()
+  .min(2, { message: "Must be at least 2 characters long." })
+  .max(50, { message: "Cannot exceed 50 characters." })
+  .regex(/^[a-zA-Z-' ]+$/, {
+    message:
+      "Only letters, spaces, hyphens (-), and apostrophes (') are allowed.",
+  });
+
+export const emailValidator = z
+  .string()
+  .trim()
+  .email({ message: "Enter a valid email address." })
+  .max(100, { message: "Email cannot exceed 100 characters." });
+
+export const passwordValidator = z
+  .string()
+  .trim()
+  .min(8, { message: "Password must be at least 8 characters long." })
+  .max(64, { message: "Password cannot exceed 64 characters." })
+  .regex(/[A-Z]/, {
+    message: "Must include at least one uppercase letter (A-Z).",
+  })
+  .regex(/[a-z]/, {
+    message: "Must include at least one lowercase letter (a-z).",
+  })
+  .regex(/\d/, { message: "Must include at least one number (0-9)." })
+  .regex(/[@$!%*?&]/, {
+    message: "Must include at least one special character (@$!%*?&).",
+  });
+
+export const tncValidator = z.boolean().refine((val) => val === true, {
+  message: "You must agree to the Terms and Conditions.",
+});
