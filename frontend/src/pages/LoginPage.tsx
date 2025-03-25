ce63142fc98b744cee23583bd0aa70b591b4f3d1
RAKESH13595 <47477434+RAKESH13595@users.noreply.github.com>
2025-03-25 05:38:56 +0530

    OAuth redirection

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -24,7 +24,10 @@ export default function LoginPage() {
   }, [user, navigate]);
 
   const handleGoogleLogin = () => {
-    window.open("http://localhost:5000/api/auth/google", "_self");
+    window.open(
+      "https://to-do-test-cjnr.onrender.com/api/auth/google",
+      "_self"
+    );
   };
   return (
     <>
