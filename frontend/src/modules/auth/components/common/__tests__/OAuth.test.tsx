72da41e46d40d2c396dbb49dd98916a35c250bbf
RAKESH13595 <47477434+RAKESH13595@users.noreply.github.com>
2025-03-30 16:46:49 +0530

    OAuth unit test cases

---
+++ frontend/src/modules/auth/components/common/__tests__/OAuth.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import OauthProvider from "../OAuth";
+import { API_BASE_URL } from "@/configs/appConfig";
+
+describe("OAuth component", () => {
+  test("should verify google auth page is opened on click of Google button", () => {
+    const windowOpenSpy = vi.spyOn(window, "open");
+    render(<OauthProvider />);
+    screen.getByTestId("signin-google-auth").click();
+    expect(windowOpenSpy).toHaveBeenCalledWith(
+      `${API_BASE_URL}/api/auth/google`,
+      "_self"
+    );
+  });
+  test("should verify microsoft auth page is opened on click of Google button", () => {
+    const windowOpenSpy = vi.spyOn(window, "open");
+    render(<OauthProvider />);
+    screen.getByTestId("signin-ms-auth").click();
+    expect(windowOpenSpy).toHaveBeenCalledWith(
+      `${API_BASE_URL}/api/auth/microsoft`,
+      "_self"
+    );
+  });
+});
